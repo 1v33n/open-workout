b66c5154170efc9252ca501843a00a598a52d939
jha129623 <jha129623@iet-gibb.ch>
2022-07-07 11:35:08 +0200

    Commit design

--- app/src/main/java/ch/bbcag/openworkout/NewWorkoutActivity.java
+++ app/src/main/java/ch/bbcag/openworkout/NewWorkoutActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ public class NewWorkoutActivity extends AppCompatActivity {
     private ImageView imageViewHome; //
     private ImageView imageViewStatistics; //
     private ImageView addNewSet;
+    private Button canelButton, saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,9 @@ public class NewWorkoutActivity extends AppCompatActivity {
         imageViewHome = findViewById(R.id.home_button);
         imageViewStatistics = findViewById(R.id.stats_button);
         addNewSet = findViewById(R.id.newSet);
+        canelButton = findViewById(R.id.canel_button);
+        saveButton = findViewById(R.id.save_button);
+
 
         imageViewHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,19 +45,35 @@ public class NewWorkoutActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-/*
+
+
         addNewSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView = new TextView(this);
+                TextView textView = new TextView(getApplicationContext());
                 textView.setGravity(Gravity.CENTER_VERTICAL);
                 textView.setText("New TextView");
 
-                RelativeLayout layout = (RelativeLayout)findViewByID(R.id.layout_id);
-                layout.addView(textView);
+                //RelativeLayout layout = (RelativeLayout)findViewByID(R.id.layout_id);
+                //layout.addView(textView);
             }
         });
-        */
+
+
+        canelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
